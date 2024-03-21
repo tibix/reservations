2eb8fe982c8c8fa72312b401043639f2d5218b9a
Dascal Tiberiu Claudiu <tiberiu.purcel@gmail.com>
2024-03-21 17:05:38 +0200

    Finished the app

--- routes/web.php
+++ routes/web.php
@@ -8,19 +8,19 @@ use App\Http\Controllers\ActivityController;
 use App\Http\Controllers\MyActivityController;
 use App\Http\Controllers\CompanyUserController;
 use App\Http\Controllers\CompanyGuideController;
+use App\Http\Controllers\GuideActivityController;
 use App\Http\Controllers\CompanyActivityController;
 use App\Http\Controllers\ActivityRegisterController;
 
 Route::get('/', HomeController::class)->name('home');
 Route::get('/activities/{activity}', [ActivityController::class, 'show'])->name('activity.show');
-Route::delete('/activities/{activity}', [MyActivityController::class, 'destroy'])->name('my_activity.destroy');
 Route::post('/activities/{activity}/register', [ActivityRegisterController::class, 'store'])->name('activities.register');
-Route::get('/info', function(){
-    phpinfo();
-});
 
 Route::middleware('auth')->group(function () {
     Route::get('/activities', [MyActivityController::class, 'show'])->name('my_activity.show');
+    Route::get('/guides/activities', [GuideActivityController::class, 'show'])->name('guide-activity.show');
+    Route::get('/guides/activities/{activity}/pdf', [GuideActivityController::class, 'export'])->name('guide-activity.export');
+    Route::delete('/activities/{activity}', [MyActivityController::class, 'destroy'])->name('my_activity.destroy');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
